43a0f49022ee3e3689f9a3175a623b3de9b5c8a2
menbuas <menbuas69@gmail.com>
2025-04-28 13:25:58 +0200

    simplify test file

--- test_seb.py
+++ test_seb.py
@@ -16,21 +16,9 @@ def compute_next_state(current_state, weights, return_h=False):
     h = weights @ current_state
     return h if return_h else np.sign(h)
 
-def compute_next_state_with_overlaps_loops_1(current_state, patterns, return_h=False):
-    h = np.zeros(current_state.shape)
-    overlaps = np.zeros(patterns.shape[0])
-    N = len(current_state)
-    P = len(patterns)
 
-    for mu in range(P):
-        overlaps[mu] = np.sum([patterns[mu,i]*current_state[i] for i in range(N)])/N
 
-    for i in range(N):
-        h[i] = np.sum([overlaps[mu]*patterns[mu,i] for mu in range(P)])
-
-    return h if return_h else np.sign(h)
-
-def compute_next_state_with_overlaps_loops_2(current_state, patterns, return_h=False):
+def compute_next_state_with_overlaps_loops(current_state, patterns, return_h=False):
     h = np.zeros(current_state.shape)
     overlaps = np.zeros(patterns.shape[0])
     N = len(current_state)
@@ -47,19 +35,20 @@ def compute_next_state_with_overlaps_loops_2(current_state, patterns, return_h=F
 
     return h if return_h else np.sign(h)
 
+
+
 NETWORK_DIM = 1340
 c = 0.134
 n_patterns = int(c * NETWORK_DIM)
 
 patterns = np.random.binomial(1, 0.5, size=(n_patterns, NETWORK_DIM)) * 2 - 1
 current_state = np.random.binomial(1, 0.5, size=NETWORK_DIM) * 2 - 1
+
 weights = get_weights(patterns)
-
 ns_standard_h = compute_next_state(current_state, weights, return_h=True)
-ns_overlap_1_h = compute_next_state_with_overlaps_loops_1(current_state, patterns, return_h=True)
-ns_overlap_2_h = compute_next_state_with_overlaps_loops_2(current_state, patterns, return_h=True)
 
-assert np.allclose(ns_overlap_1_h, ns_overlap_2_h), "Overlap 1 and overlap 2 methods do not match"
+ns_overlap_h = compute_next_state_with_overlaps_loops(current_state, patterns, return_h=True)
 
-plt.hist(ns_standard_h - ns_overlap_1_h, bins=100)
+
+plt.hist(ns_standard_h - ns_overlap_h, bins=100)
 plt.show()
